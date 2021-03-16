864c4e35594f186eb8fe33cb0146eaaf7c06470a
Ignacio Andrés Jorquera Ferrat <igjorque@unirioja.es>
2021-03-16 20:41:35 +0100

    Completado

---
+++ app/src/main/java/android/example/roomwordssample/WordDao.java
@@ -0,0 +1,18 @@
+package android.example.roomwordssample;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
